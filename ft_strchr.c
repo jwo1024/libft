eeca2fba7e4d786e862aeae3ecee822d0d4cb385
jiwolee <jiwolee@student.42seoul.kr>
2022-01-19 22:00:04 +0900

    hi

--- ft_strchr.c
+++ ft_strchr.c
@@ -7,11 +7,11 @@ char	*ft_strchr(const char *s, int c)
 	a = (char)c;
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == a)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == '\0' && c == '\0')
+	if (*s == '\0' && a == '\0')
 		return ((char *)s);
 	return (NULL);
 }
